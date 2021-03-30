9085f6be72cd44cc148de29c478ccf7434b66aa5
dodoongtak <themighty.tak@gmail.com>
2021-03-30 19:30:39 +0900

    ♻️ Increase readabily with context of isNew
    Use optional chaing and guard clause
    
    See Also:
    - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
    - https://refactoring.com/catalog/replaceNestedConditionalWithGuardClauses.html

--- src/screens/HomeScreen/Home.jsx
+++ src/screens/HomeScreen/Home.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
 export default function Home({ profile, onClick }) {
+  if (!profile.isNew) {
+    return (
+      <>
+        <p>
+          {profile.name}
+          님 안녕하세요!
+        </p>
+
+        <a
+          href="/profile"
+          onClick={() => onClick({ url: '/profile"' })}
+        >
+          내 정보 확인하러가기
+        </a>
+      </>
+    );
+  }
+
   return (
     <div>
       <h2>꿈꾸는 삶을 살기 위해 얼마나 많은 돈을 더 벌어야 될까요?</h2>
       {
-        profile && profile.isNew ? (
+        profile?.isNew && (
           <a
             href="/profile/new"
             onClick={() => onClick({ url: '/profile/new' })}
@@ -13,21 +31,6 @@ export default function Home({ profile, onClick }) {
             내 정보 입력하러가기
           </a>
         )
-          : (
-            <>
-              <p>
-                {profile.name}
-                님 안녕하세요!
-              </p>
-
-              <a
-                href="/profile"
-                onClick={() => onClick({ url: '/profile"' })}
-              >
-                내 정보 확인하러가기
-              </a>
-            </>
-          )
       }
     </div>
   );
