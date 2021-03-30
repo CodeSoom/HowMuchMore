cdb9b992a2f3247d45c92d85aa9bb6a62816c18e
dodoongtak <themighty.tak@gmail.com>
2021-03-30 19:45:23 +0900

    ♻️ convert profile elements to description elements
    Awesome Semantic
    
    See Also:
    - https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dl

--- src/screens/MyPageScreen/MyPage.jsx
+++ src/screens/MyPageScreen/MyPage.jsx
@@ -1,47 +1,37 @@
 import React from 'react';
 
 export default function MyPage({ profile, onClickNewProfile }) {
+  if (profile.isNew) {
+    return (
+      <>
+        <p>
+          아직 정보를 입력하지 않으셨습니다.
+        </p>
+        <a
+          href="/profile/new"
+          onClick={() => onClickNewProfile()}
+        >
+          내 정보 입력하러가기
+        </a>
+      </>
+    );
+  }
+
   return (
     <div>
       {
-        profile && profile.isNew ? (
+        !profile.isNew && (
           <>
-            <p>
-              아직 정보를 입력하지 않으셨습니다.
-            </p>
-            <a
-              href="/profile/new"
-              onClick={() => onClickNewProfile()}
-            >
-              내 정보 입력하러가기
-            </a>
-          </>
-        ) : (
-          <>
-            <p>
-              <span>
-                이름:
-              </span>
-              {profile.name}
-            </p>
-            <p>
-              <span>
-                나이:
-              </span>
-              {profile.age}
-            </p>
-            <p>
-              <span>
-                연봉:
-              </span>
-              {profile.salary}
-            </p>
-            <p>
-              <span>
-                자산:
-              </span>
-              {profile.asset}
-            </p>
+            <dl>
+              <dd>이름:</dd>
+              <dt>{profile.name}</dt>
+              <dd>나이:</dd>
+              <dt>{profile.age}</dt>
+              <dd>연봉:</dd>
+              <dt>{profile.salary}</dt>
+              <dd>자산:</dd>
+              <dt>{profile.asset}</dt>
+            </dl>
           </>
         )
       }
