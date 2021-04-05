926e4119b77c12e1e4b99d27141d8c4cb89c5d9a
dodoongtak <themighty.tak@gmail.com>
2021-04-06 04:12:14 +0900

    ✅ test: add scenario for testing fetchApartment returns list of apartments

--- src/services/__mocks__/api.js
+++ src/services/__mocks__/api.js
@@ -1,5 +1,20 @@
 export async function fetchApartments() {
-  return [];
+  return [
+    {
+      name: '아크로리버파크',
+      date: '2021-03',
+      area: '129.92',
+      price: '470,000',
+      lotNumber: 1,
+    },
+    {
+      name: '서울',
+      date: '2021-02',
+      area: '200.27',
+      price: '420,000',
+      lotNumber: 2,
+    },
+  ];
 }
 
 // TODO: TO BE DELETED
