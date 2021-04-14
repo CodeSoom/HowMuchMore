93fb5c023edea86495b63229c00cc9658ddcc153
dodoongtak <themighty.tak@gmail.com>
2021-04-14 12:16:43 +0900

    ✨ feat: Update additional apartments types
    Updated apartment types:
    - commercial
    - landscape
    - commute
      - gangnam
      - yeouido
      - gwanghwamun

--- src/pages/Apartment/ApartmentPage.test.jsx
+++ src/pages/Apartment/ApartmentPage.test.jsx
@@ -42,10 +42,10 @@ describe('ApartmentPage', () => {
     expect(screen.getByText('거주하고 싶은신 아파트를 선택해주세요')).toBeInTheDocument();
 
     expect(screen.getByRole('link', {
-      name: '한강 뷰',
+      name: /한강 뷰/,
     })).toBeInTheDocument();
     expect(screen.getByRole('link', {
-      name: '우수 상권',
+      name: /상권/,
     })).toBeInTheDocument();
 
     expect(screen.getByText('아크로리버파크')).toBeInTheDocument();
