64a55822b79a31e5f304c0d38f32178014e52169
Tak <77006427+dodoongtak@users.noreply.github.com>
2021-05-05 23:46:06 +0900

    Revert "UIUX 💄: Adjust HomeLayout overlaying footer"

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,7 +12,6 @@ import { LinkField } from '../../commons/Fields';
 import { Footer } from '../../commons/Footer';
 
 const HomeLayout = styled.section({
-  minHeight: '80vh',
   display: 'flex',
   flexDirection: 'column',
 });
